e21570f28991526ddd1070d892d08f95d5788c88
Mostafa <Mostafagholipore@gmail.com>
2023-10-17 18:27:49 +0330

    Creat BaseEntity

---
+++ src/main/java/com/example/SmartPath/SmartPathApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SmartPath/SmartPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
