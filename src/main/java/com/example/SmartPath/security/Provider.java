5ba25f875f4a31f652ae98e3bdb1a8b9a301bd0d
Mostafa <Mostafagholipore@gmail.com>
2023-10-17 23:03:38 +0330

    Create Provider

---
+++ src/main/java/com/example/SmartPath/security/Provider.java
@@ -0,0 +1,34 @@
+package com.example.SmartPath.security;
+
+import com.example.SmartPath.base.Person;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Provider implements AuthenticationProvider {
+    private final CustomDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        Person person = (Person) customUserDetailsService.loadUserByUsername(authentication.getName());
+        if (passwordEncoder.matches(((String) authentication.getCredentials()), person.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(
+                    person, null, person.getAuthorities()
+            );
+        }
+        throw new BadCredentialsException("wrong information");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
