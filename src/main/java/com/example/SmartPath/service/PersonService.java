1a783b4ae15bb427fd15228f27dd3ec06d5f5b4a
Mostafa <Mostafagholipore@gmail.com>
2023-10-17 19:42:13 +0330

    Create PersonService and PersonServiceImpl

---
+++ src/main/java/com/example/SmartPath/service/PersonService.java
@@ -0,0 +1,4 @@
+package com.example.SmartPath.service;
+
+public interface PersonService {
+}
---
+++ src/main/java/com/example/SmartPath/service/impl/PersonServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.SmartPath.service.impl;
+
+import com.example.SmartPath.service.PersonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PersonServiceImpl implements PersonService {
+
+}
