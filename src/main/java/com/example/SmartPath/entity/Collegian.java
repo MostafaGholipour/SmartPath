fe091d976d564792268bad6457f1d31608f77e1b
Mostafa <Mostafagholipore@gmail.com>
2023-10-18 07:09:16 +0330

    add Constructor in Collegian

--- src/main/java/com/example/SmartPath/entity/Collegian.java
+++ src/main/java/com/example/SmartPath/entity/Collegian.java
@@ -1,6 +1,7 @@
 package com.example.SmartPath.entity;
 
 import com.example.SmartPath.entity.base.Person;
+import com.example.SmartPath.entity.enums.UserRole;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -21,4 +22,8 @@ public class Collegian extends Person {
     @ManyToMany(mappedBy = "personList",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
     List<Course> courseList=new ArrayList<>();
 
+    public Collegian(String firstName, String lastName,
+                     String username, String password) {
+        super(firstName, lastName, username, password, UserRole.COLLEGIAN, false);
+    }
 }
