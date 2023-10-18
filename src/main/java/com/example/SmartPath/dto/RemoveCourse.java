1d459ba991462a866ce2479ee4b6636c6f8b3514
Mostafa <Mostafagholipore@gmail.com>
2023-10-18 12:00:03 +0330

    Create RemoveCourse

---
+++ src/main/java/com/example/SmartPath/dto/RemoveCourse.java
@@ -0,0 +1,13 @@
+package com.example.SmartPath.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RemoveCourse {
+    String username;
+    String CourseName;
+}
