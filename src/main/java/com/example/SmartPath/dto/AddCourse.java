acafe9bf54856505f03199841570f192ba956f13
Mostafa <Mostafagholipore@gmail.com>
2023-10-18 11:55:21 +0330

    Create AddCourse

---
+++ src/main/java/com/example/SmartPath/dto/AddCourse.java
@@ -0,0 +1,13 @@
+package com.example.SmartPath.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddCourse {
+    String username;
+    String courseName;
+}
