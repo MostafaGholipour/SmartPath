9959ebe51e78952b4577d6ada9c36f218b60bb9a
Mostafa <Mostafagholipore@gmail.com>
2023-10-18 06:38:24 +0330

    Create CourseService

---
+++ src/main/java/com/example/SmartPath/service/impl/CourseServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.SmartPath.service.impl;
+
+import com.example.SmartPath.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CourseServiceImpl implements CourseService {
+}
