3c14b6fc861de94705c3c2c5419efd13ceb9bb0a
Mostafa <Mostafagholipore@gmail.com>
2023-10-18 11:57:58 +0330

    Create ManagerRepository

--- src/main/java/com/example/SmartPath/repository/ManagerRepository.java
+++ src/main/java/com/example/SmartPath/repository/ManagerRepository.java
@@ -2,6 +2,8 @@ package com.example.SmartPath.repository;
 
 import com.example.SmartPath.entity.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ManagerRepository extends JpaRepository<Manager,Long> {
 }
