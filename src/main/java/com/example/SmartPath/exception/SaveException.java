c3be225df797ab6952a3e2fe69b592a6e9b298dd
Mostafa <Mostafagholipore@gmail.com>
2023-10-18 11:59:16 +0330

    add SaveException GlobalExceptionHandler

---
+++ src/main/java/com/example/SmartPath/exception/SaveException.java
@@ -0,0 +1,5 @@
+package com.example.SmartPath.exception;
+
+public class SaveException extends RuntimeException{
+    public SaveException(String s){super(s);}
+}
--- src/main/java/com/example/SmartPath/exception/global/GlobalExceptionHandler.java
+++ src/main/java/com/example/SmartPath/exception/global/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.SmartPath.exception.global;
 
 import com.example.SmartPath.exception.NotFoundException;
+import com.example.SmartPath.exception.SaveException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity serviceExistHandler(NotFoundException e){
+        String response =e.getMessage();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(SaveException.class)
+    public ResponseEntity serviceExistHandler(SaveException e){
         String response =e.getMessage();
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
